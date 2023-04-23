e544e23c0489c732a114be6a26a83eb5041b399a
Maokli <louayinfo07@gmail.com>
2023-04-23 02:06:17 +0100

    created database schema

---
+++ api/src/Entity/Language.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LanguageRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LanguageRepository::class)]
+class Language
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\OneToMany(mappedBy: 'language', targetEntity: Quizz::class)]
+    private Collection $quizzs;
+
+    public function __construct()
+    {
+        $this->quizzs = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Quizz>
+     */
+    public function getQuizzs(): Collection
+    {
+        return $this->quizzs;
+    }
+
+    public function addQuizz(Quizz $quizz): self
+    {
+        if (!$this->quizzs->contains($quizz)) {
+            $this->quizzs->add($quizz);
+            $quizz->setLanguage($this);
+        }
+
+        return $this;
+    }
+
+    public function removeQuizz(Quizz $quizz): self
+    {
+        if ($this->quizzs->removeElement($quizz)) {
+            // set the owning side to null (unless already changed)
+            if ($quizz->getLanguage() === $this) {
+                $quizz->setLanguage(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ api/src/Entity/Quizz.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuizzRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: QuizzRepository::class)]
+class Quizz
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 1000)]
+    private ?string $questions = null;
+
+    #[ORM\Column(length: 1000)]
+    private ?string $answers = null;
+
+    #[ORM\ManyToOne(inversedBy: 'quizzs')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Language $language = null;
+
+    #[ORM\ManyToOne(inversedBy: 'quizzs')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Category $category = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getQuestions(): ?string
+    {
+        return $this->questions;
+    }
+
+    public function setQuestions(string $questions): self
+    {
+        $this->questions = $questions;
+
+        return $this;
+    }
+
+    public function getAnswers(): ?string
+    {
+        return $this->answers;
+    }
+
+    public function setAnswers(string $answers): self
+    {
+        $this->answers = $answers;
+
+        return $this;
+    }
+
+    public function getLanguage(): ?Language
+    {
+        return $this->language;
+    }
+
+    public function setLanguage(?Language $language): self
+    {
+        $this->language = $language;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
