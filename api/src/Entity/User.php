94e650a0e3ac51c7714822bd0d39166fd0e86d96
Maokli <louayinfo07@gmail.com>
2023-04-23 02:06:17 +0100

    created database schema

---
+++ api/src/Entity/User.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $firstName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lastName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $passwordHash = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $passwordSalt = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $preferredLanguages = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPasswordHash(): ?string
+    {
+        return $this->passwordHash;
+    }
+
+    public function setPasswordHash(string $passwordHash): self
+    {
+        $this->passwordHash = $passwordHash;
+
+        return $this;
+    }
+
+    public function getPasswordSalt(): ?string
+    {
+        return $this->passwordSalt;
+    }
+
+    public function setPasswordSalt(string $passwordSalt): self
+    {
+        $this->passwordSalt = $passwordSalt;
+
+        return $this;
+    }
+
+    public function getPreferredLanguages(): ?string
+    {
+        return $this->preferredLanguages;
+    }
+
+    public function setPreferredLanguages(string $preferredLanguages): self
+    {
+        $this->preferredLanguages = $preferredLanguages;
+
+        return $this;
+    }
+}
