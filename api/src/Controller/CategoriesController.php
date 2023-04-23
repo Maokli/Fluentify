f5fb5a0a5459f4590a0106074b9f2ad79d623848
Maokli <louayinfo07@gmail.com>
2023-04-23 14:48:16 +0100

    add and get categories

---
+++ api/src/Controller/CategoriesController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
+
+#[Route('/api', name: 'app_auth')]
+class CategoriesController extends AbstractController
+{
+    #[Route('/categories', name: 'app_categories', methods: "GET")]
+    public function index(ManagerRegistry $doctrine): JsonResponse
+    {
+        // get the entities from DB
+        $entityManager = $doctrine->getManager();
+        $categories = $entityManager->getRepository(Category::class)->findAll();
+
+        // we map them to a dto
+        $data = [];
+        foreach ($categories as $category) {
+            $data[] = [
+                'id' => $category->getId(),
+                'name' => $category->getName(),
+            ];
+        }
+
+        return $this->json($data);
+    }
+
+    //TODO: make this available to admins only
+    #[Route('/admin/categories/add', name: 'add_categoriy', methods: "POST")]
+    public function add(ManagerRegistry $doctrine, Request $request): JsonResponse
+    {
+        // get the entities from DB
+        $entityManager = $doctrine->getManager();
+
+        //extract request body
+        $body = json_decode($request->getContent());
+        $name = $body->name;
+        // create entity
+        $category = new Category();
+        $category->setName($name);
+        // save
+        $entityManager->persist($category);
+        $entityManager->flush();
+
+        return $this->json(["message" => "Added Successfully"]);
+    }
+}
