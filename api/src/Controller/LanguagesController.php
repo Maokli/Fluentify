5b9dc7c9186ca9cfe946ec95aa3831f339f5a3da
Maokli <louayinfo07@gmail.com>
2023-04-23 15:00:23 +0100

    language controller

---
+++ api/src/Controller/LanguagesController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Language;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/api')]
+class LanguagesController extends AbstractController
+{
+    #[Route('/languages', methods:"GET")]
+    public function index(ManagerRegistry $doctrine): JsonResponse
+    {
+        // get the entities from DB
+        $entityManager = $doctrine->getManager();
+        $languages = $entityManager->getRepository(Language::class)->findAll();
+
+        // we map them to a dto
+        $data = [];
+        foreach ($languages as $language) {
+            $data[] = [
+                'id' => $language->getId(),
+                'name' => $language->getName(),
+            ];
+        }
+
+        return $this->json($data);
+    }
+
+    //TODO: make this available to admins only
+    #[Route('/admin/languages/add', methods: "POST")]
+    public function add(ManagerRegistry $doctrine, Request $request): JsonResponse
+    {
+        // get the entities from DB
+        $entityManager = $doctrine->getManager();
+
+        //extract request body
+        $body = json_decode($request->getContent());
+        $name = $body->name;
+        // create entity
+        $language = new Language();
+        $language->setName($name);
+        // save
+        $entityManager->persist($language);
+        $entityManager->flush();
+
+        return $this->json(["message" => "Added Successfully"]);
+    }
+}
