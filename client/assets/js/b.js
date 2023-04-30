17e979d649e409ff5d455ac96ed9523f804b2d0b
Hatem Gharsallah <hatem.gharsallah@insat.ucar.tn>
2023-04-30 08:24:06 +0100

    linking login and sign up to the api

--- client/assets/js/b.js
+++ client/assets/js/b.js
@@ -1,14 +1,16 @@
-$(document).ready(function() {
-    $("#show_hide_password a").on('click', function(event) {
-        event.preventDefault();
-        if($('#show_hide_password input').attr("type") == "text"){
-            $('#show_hide_password input').attr('type', 'password');
-            $('#show_hide_password i').addClass( "fa-eye-slash" );
-            $('#show_hide_password i').removeClass( "fa-eye" );
-        }else if($('#show_hide_password input').attr("type") == "password"){
-            $('#show_hide_password input').attr('type', 'text');
-            $('#show_hide_password i').removeClass( "fa-eye-slash" );
-            $('#show_hide_password i').addClass( "fa-eye" );
-        }
-    });
-});
+function password_show_hide() {
+    var x = document.getElementById("password");
+    var show_eye = document.getElementById("show_eye");
+    var hide_eye = document.getElementById("hide_eye");
+    hide_eye.classList.remove("d-none");
+    show_eye.classList.add("d-block");
+    if (x.type === "password") {
+        x.type = "text";
+        show_eye.style.display = "none";
+        hide_eye.style.display = "block";
+    } else {
+        x.type = "password";
+        show_eye.style.display = "block";
+        hide_eye.style.display = "none";
+    }
+}
--- client/scripts/login.js
+++ client/scripts/login.js
@@ -1,13 +1,6 @@
 function login(email, password) {
   var myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
-  /*
-  "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods":
-              "POST, PUT, PATCH, GET, DELETE, OPTIONS",
-            "Access-Control-Allow-Headers":
-              "Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization",
-  */
   myHeaders.append("Access-Control-Allow-Origin", "*");
   myHeaders.append("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE");
   myHeaders.append("Access-Control-Allow-Headers", "*");
@@ -27,6 +20,12 @@ function login(email, password) {
   fetch("http://127.0.0.1:8000/api/login_check", requestOptions)
     .then((response) => response.json())
     .then((result) => localStorage.setItem("token", result.token))
+    .then((headers)=> {
+        if (headers.status == 200) {
+          // after login page redirect to welcome page 
+            window.location.href = "welcome.html";
+        }
+    })
     .catch((error) => console.log("error", error));
 }
 function submitForm(event) {
