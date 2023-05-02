7a07d31c08df79a7b8c1871669b114ad63bc919e
Maokli <louayinfo07@gmail.com>
2023-05-02 22:58:30 +0100

    editing quizz entity and adding an endpoint to add a quizz

--- api/src/Controller/QuizzController.php
+++ api/src/Controller/QuizzController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Category;
+use App\Entity\Quizz;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,6 +10,31 @@ use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Persistence\ManagerRegistry;
 
 #[Route('/api')]
-class QuizzController extends AbstractController{
+class QuizzController extends AbstractController
+{
+
+  //TODO: make this available to admins only
+  #[Route('/admin/quizz/add', methods: "POST")]
+  public function add(ManagerRegistry $doctrine, Request $request): JsonResponse
+  {
+      // get the entities from DB
+      $entityManager = $doctrine->getManager();
+
+      //extract request body
+      $body = json_decode($request->getContent());
+      $categoryId = $body->categoryId;
+      $languageId = $body->languageId;
+      $questions = $body->questions;
+      $options = $body->options;
+      $answers = $body->answers;
+      // create entity
+      $auizz = new Quizz();
+
+      // save
+      $entityManager->persist($auizz);
+      $entityManager->flush();
+
+      return $this->json(["message" => "Added Successfully"]);
+  }
 
 }
