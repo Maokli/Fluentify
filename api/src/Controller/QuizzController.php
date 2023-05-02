c045379de160d6a772cd9f0b9562ac69fa8de674
Hatem Gharsallah <hatem.gharsallah@insat.ucar.tn>
2023-05-02 22:39:32 +0100

    add quizz controller

---
+++ api/src/Controller/QuizzController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
+
+#[Route('/api')]
+class QuizzController extends AbstractController{
+
+}
