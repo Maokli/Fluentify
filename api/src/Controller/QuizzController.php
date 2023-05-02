c16ddbae2c5fe5734706f4ca0d410b404ab91a43
Maokli <louayinfo07@gmail.com>
2023-05-02 22:58:30 +0100

    editing quizz entity and adding an endpoint to add a quizz

--- api/src/Controller/QuizzController.php
+++ api/src/Controller/QuizzController.php
@@ -1,9 +1,8 @@
 <?php
 
 namespace App\Controller;
-use App\Entity\Quizz;
 
-use App\Entity\Category;
+use App\Entity\Quizz;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +12,30 @@ use Doctrine\Persistence\ManagerRegistry;
 #[Route('/api')]
 class QuizzController extends AbstractController
 {
+
+  //TODO: make this available to admins only
+  #[Route('/admin/quizz/add', methods: "POST")]
+  public function add(ManagerRegistry $doctrine, Request $request): JsonResponse
+  {
+      // get the entities from DB
+      $entityManager = $doctrine->getManager();
+
+      //extract request body
+      $body = json_decode($request->getContent());
+      $categoryId = $body->categoryId;
+      $languageId = $body->languageId;
+      $questions = $body->questions;
+      $options = $body->options;
+      $answers = $body->answers;
+      // create entity
+      $auizz = new Quizz();
+
+      // save
+      $entityManager->persist($auizz);
+      $entityManager->flush();
+
+      return $this->json(["message" => "Added Successfully"]);
+  }
 
     #[Route('/quizz/{id}', methods:"GET")]
     public function getQuizzById(ManagerRegistry $doctrine, Request $request, int $id) : JsonResponse
