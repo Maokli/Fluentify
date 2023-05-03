97d711e3c0351a016fa58d51a2731402fa78ef8e
DogWitDaButter <seifjeder0@gmail.com>
2023-05-03 05:41:12 +0100

    Added an end point to fetch/add favorites

--- api/src/Controller/LanguagesController.php
+++ api/src/Controller/LanguagesController.php
@@ -3,11 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Language;
+use App\Entity\User;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Helpers;
 
 #[Route('/api')]
 class LanguagesController extends AbstractController
@@ -56,4 +58,62 @@ class LanguagesController extends AbstractController
 
         return $this->json(["message" => "Added Successfully"]);
     }
+    #[Route('/languages/Favorite', methods:"GET")]
+    public function getFavorite(ManagerRegistry $doctrine):JsonResponse
+    {
+        $entityManager = $doctrine->getManager();
+        $headers = apache_request_headers();
+        $token = $headers['Authorization'];
+        $userInDb = Helpers\getUserFromToken($entityManager, $token);
+        if($userInDb == null)
+            return $this->json([
+                'Error' => 'No user exists with this Email',
+            ], 404);
+        $userLanguages = explode("/",$userInDb->getPreferredLanguages());
+        $userLanguages = array_filter($userLanguages, function($value) {
+            return !empty($value);
+        });
+        $data = [];
+        foreach ($userLanguages as $userLanguage) {
+            $languageInDb = $entityManager->getRepository(Language::class)->findOneById($userLanguage);
+            if ($languageInDb) {
+                $data[]= [
+                    "languageId" => $languageInDb->getId(),
+                    "languageName" => $languageInDb->getName(),
+                    "photoUrl" => $languageInDb->getPhotoURl(),
+                    "description" => $languageInDb->getDescription(),
+                ];
+            }
+        }
+        return $this->json($data);
+    }
+    #[Route('/languages/Favorite', methods: "POST")]
+public function setFavorite(ManagerRegistry $doctrine, Request $request): JsonResponse
+{
+    $entityManager = $doctrine->getManager();
+    $headers = apache_request_headers();
+    $token = $headers['Authorization'];
+    $userInDb = Helpers\getUserFromToken($entityManager, $token);
+    if ($userInDb == null) {
+        return $this->json([
+            'Error' => 'No user exists with this Email',
+        ], 404);
+    }
+    $body = json_decode($request->getContent());
+    $newLanguageId = $body->id;
+    $userLanguages = explode("/", $userInDb->getPreferredLanguages());
+    $userLanguages = array_filter($userLanguages, function($value) {
+        return !empty($value);
+    });
+    // Only add new language ID if it doesn't already exist in the list
+    if (!in_array($newLanguageId, $userLanguages)) {
+        $userLanguages[] = $newLanguageId;
+        $userInDb->setPreferredLanguages(implode("/", $userLanguages));
+        $entityManager->persist($userInDb);
+        $entityManager->flush();
+    }
+    return $this->json(["message" => "Added Successfully"]);
+}
+
+
 }
