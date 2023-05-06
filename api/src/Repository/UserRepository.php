30b5eb264271e4544eb137bc52ca3698706ee78d
Hatem Gharsallah <hatem.gharsallah@insat.ucar.tn>
2023-05-07 00:40:35 +0100

    add solved quizzes to the user

--- api/src/Repository/UserRepository.php
+++ api/src/Repository/UserRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\User;
+use App\Entity\UserQuizz;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
