4ec10c5329072634910daa6d7e5b9e718f0be310
DogWitDaButter <seifjeder0@gmail.com>
2023-05-09 21:58:34 +0100

    Added some animations and fixes

--- client/scripts/assistant.js
+++ client/scripts/assistant.js
@@ -12,8 +12,42 @@ async function getUserLanguages() {
 
   //injecting each card int the card container
   languages.forEach((language) => {
-    USER_LANGUAGES.push(language.languageName);
+    USER_LANGUAGES.push(language);
   });
+}
+
+function createLanguageCard(photo, languageName) {
+  const col = document.createElement("div");
+  col.className = "col-md-3 mb-4";
+
+  const card = document.createElement("div");
+  card.className = "card hoverable h-100";
+  card.style = "width: 13rem; height: 10rem;";
+
+  const imgContainer = document.createElement("div");
+  imgContainer.className = "d-flex justify-content-center";
+
+  const img = document.createElement("img");
+  img.className = "card-img-top rounded-start flags text-center";
+  img.src = photo;
+  img.alt = `${languageName} flag`;
+
+  imgContainer.appendChild(img);
+  card.appendChild(imgContainer);
+
+  const body = document.createElement("div");
+  body.className = "card-body d-flex flex-column justify-content-between";
+
+  const title = document.createElement("h5");
+  title.className = "card-title text-center mt-3";
+  title.innerText = languageName;
+
+  body.appendChild(title);
+  card.appendChild(body);
+
+  col.appendChild(card);
+
+  return col;
 }
 
 function getCard(text) {
@@ -27,13 +61,18 @@ function getCard(text) {
 
   const col = document.createElement("div");
   col.className = "col";
+  const banner=document.createElement("div");
+  banner.className="banner";
   const card = document.createElement("div");
-  card.className = "card hoverable px-3 py-5";
+  card.className = "card hoverable px-3 py-5 animated-card";
   const title = document.createElement("h2");
   title.className = "text-center";
   title.innerText = text;
+  title.style.zIndex = 2;
+  
 
   card.appendChild(title);
+  card.appendChild(banner);
   col.appendChild(card);
 
   return col;
@@ -45,10 +84,10 @@ async function renderLanguageSelection() {
   const container = document.createElement("div");
   container.className = "row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4";
   USER_LANGUAGES.forEach((language) => {
-    const languageCard = getCard(language);
+    const languageCard = createLanguageCard(language.photoUrl, language.languageName);
 
     languageCard.addEventListener("click", () => {
-      window.language = language;
+      window.language = language.languageName;
       container.remove();
       renderChatSection();
     });
@@ -86,7 +125,7 @@ function renderChatSection() {
           </div>
 
 
-          <div class="ps-container ps-theme-default ps-active-y" id="chat-content" style="overflow-y: scroll !important; height:75vh !important;">
+          <div class="ps-container ps-theme-default ps-active-y" id="chat-content" style="overflow-y: scroll !important; height:67vh !important;">
             
 
           <div class="ps-scrollbar-x-rail" style="left: 0px; bottom: 0px;"><div class="ps-scrollbar-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps-scrollbar-y-rail" style="top: 0px; height: 0px; right: 2px;"><div class="ps-scrollbar-y" tabindex="0" style="top: 0px; height: 2px;"></div></div></div>
@@ -155,7 +194,7 @@ async function renderAssistantResponse() {
 
 function getAssistantHtmlMessage(message) {
   return `<div class="media media-chat">
-              <img class="avatar" src="https://img.icons8.com/color/36/000000/administrator-male.png" alt="...">
+              <img class="avatar" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVqu3SqbTk4F1Xx009sdY8ehiHDD7RthBZ1pVxtm6QAA&s" alt="...">
               <div class="media-body">
                 <p>${message}</p>
               </div>
