62aa4901592f321dd44deb1c71deca7559fbb3ae
DogWitDaButter <seifjeder0@gmail.com>
2023-05-09 16:50:51 +0100

    Document formatting

--- client/scripts/language.js
+++ client/scripts/language.js
@@ -1,4 +1,4 @@
-import axiosInstance from '../helpers/axiosInstanceClient.js';
+import axiosInstance from "../helpers/axiosInstanceClient.js";
 //creating a card for each language
 function createLanguageCard(language) {
   return `
@@ -19,8 +19,7 @@ function createLanguageCard(language) {
   `;
 }
 
-function setAsJoined(button)
-{
+function setAsJoined(button) {
   button.disabled = true;
   button.classList = "btn btn-success";
   button.textContent = "Joined";
@@ -34,36 +33,38 @@ async function AddToFavourite(languageId, button) {
     await axiosInstance.post("languages/Favorite", payload);
     // if no exceptions, we set it as joined
     setAsJoined(button);
-
   } catch (error) {
     alert("something went wrong");
   }
 }
 
-
 async function renderPage() {
-const allLanguagesResponse = await axiosInstance.get('/languages');
+  const allLanguagesResponse = await axiosInstance.get("/languages");
   //getting languages from the database
-  const languages=allLanguagesResponse.data;
-  const cardContainer= document.querySelector('#cardContainer');
+  const languages = allLanguagesResponse.data;
+  const cardContainer = document.querySelector("#cardContainer");
 
   // gettung favourite languages
-  const favouriteLanguagesResponse = await axiosInstance.get('/languages/Favorite');
-  const favouriteLanguagesIds = favouriteLanguagesResponse.data.map(language => language.languageId);
+  const favouriteLanguagesResponse = await axiosInstance.get(
+    "/languages/Favorite"
+  );
+  const favouriteLanguagesIds = favouriteLanguagesResponse.data.map(
+    (language) => language.languageId
+  );
   //injecting each card int the card container
-  languages.forEach(language=> {
+  languages.forEach((language) => {
     const card = createLanguageCard(language);
-    const cardElement = document.createRange().createContextualFragment(card).firstElementChild;
+    const cardElement = document
+      .createRange()
+      .createContextualFragment(card).firstElementChild;
     const joinBtn = cardElement.querySelector("button");
     // langyuage is already favourited
-    if(favouriteLanguagesIds.includes(language.id))
-    {
+    if (favouriteLanguagesIds.includes(language.id)) {
       setAsJoined(joinBtn);
-    }
-    else {
+    } else {
       joinBtn.addEventListener("click", () => {
         AddToFavourite(language.id, joinBtn);
-      })
+      });
     }
     cardContainer.appendChild(cardElement);
   });
